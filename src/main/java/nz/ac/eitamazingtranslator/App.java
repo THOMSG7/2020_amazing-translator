c9c35a991f530b6601d646d2c0f326e5a611b885
THOMSG7 <geetee8teen@gmail.com>
2020-08-10 21:09:12 +1200

    FRENCH AND GERMAN NUMBERS UPDATED

--- src/main/java/nz/ac/eitamazingtranslator/App.java
+++ src/main/java/nz/ac/eitamazingtranslator/App.java
@@ -15,12 +15,66 @@ public class App
         List<String> germanTranslation = new ArrayList<>();
         germanTranslation.add("Einz");
         germanTranslation.add("Zwei");
-        germanTranslation.add("Einz");
+        germanTranslation.add("Drei");
+        germanTranslation.add("Vier");
+        germanTranslation.add("Fünf");
+        germanTranslation.add("Sechs");
+        germanTranslation.add("Sieben");
+        germanTranslation.add("Acht");
+        germanTranslation.add("Neun");
+        germanTranslation.add("Zehn");
+        germanTranslation.add("Elf");
+        germanTranslation.add("Zwölf");
+        germanTranslation.add("Dreizehn");
+        germanTranslation.add("Vierzehn");
+        germanTranslation.add("Fünfzehn");
+        germanTranslation.add("Sechzehn");
+        germanTranslation.add("Siebzehn");
+        germanTranslation.add("Achtzehn");
+        germanTranslation.add("Neunzehn");
+        germanTranslation.add("Zwanzig");
+        germanTranslation.add("Einundzwanzig");
+        germanTranslation.add("Zweiundzwanzig");
+        germanTranslation.add("Dreiundzwanzig");
+        germanTranslation.add("Vierundzwanzig");
+        germanTranslation.add("Fünfundzwanzig");
+        germanTranslation.add("Sechsundzwanzig");
+        germanTranslation.add("Siebenundzwanzig");
+        germanTranslation.add("Achtundzwanzig");
+        germanTranslation.add("Neunundzwanzig");
+        germanTranslation.add("Dreiβig");
 
         List<String> frenchTranslation = new ArrayList<>();
+        frenchTranslation.add("Un");
+        frenchTranslation.add("Deux");
+        frenchTranslation.add("Trois");
+        frenchTranslation.add("Quatre");
+        frenchTranslation.add("Cinq");
+        frenchTranslation.add("Six");
+        frenchTranslation.add("Sept");
+        frenchTranslation.add("Huit");
+        frenchTranslation.add("Neuf");
+        frenchTranslation.add("Dix");
         frenchTranslation.add("Onze");
-        frenchTranslation.add("Einz");
-        frenchTranslation.add("Einz");
+        frenchTranslation.add("Douze");
+        frenchTranslation.add("Treize");
+        frenchTranslation.add("Quatorze");
+        frenchTranslation.add("Quinze");
+        frenchTranslation.add("Seize");
+        frenchTranslation.add("Dix-Sept");
+        frenchTranslation.add("Dix-Huit");
+        frenchTranslation.add("Dix-Neuf");
+        frenchTranslation.add("Vingt");
+        frenchTranslation.add("Vingt-Et-Un");
+        frenchTranslation.add("Vingt-Deux");
+        frenchTranslation.add("Vingt-Trois");
+        frenchTranslation.add("Vingt-Quatre");
+        frenchTranslation.add("Vingt-Cinq");
+        frenchTranslation.add("Vingt-Six");
+        frenchTranslation.add("Vingt-Sept");
+        frenchTranslation.add("Vingt-Huit");
+        frenchTranslation.add("Vingt-Neuf");
+        frenchTranslation.add("Trente");
 
         System.out.println("What is the number to translate?");
         Scanner scanner = new Scanner(System.in);
@@ -49,6 +103,5 @@ public class App
         if (option == 1) {
             string translation = frenchTranslation.get(number - 1);
         }
-
     }
 }
