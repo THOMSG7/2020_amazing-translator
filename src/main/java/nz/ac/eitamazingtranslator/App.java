00f3ba360f2b50412ab0b15ab6511fa6b8374654
THOMSG7 <geetee8teen@gmail.com>
2020-08-10 16:00:17 +1200

    A COUPLE OF NUMBERS IN FRENCH

--- src/main/java/nz/ac/eitamazingtranslator/App.java
+++ src/main/java/nz/ac/eitamazingtranslator/App.java
@@ -1,5 +1,9 @@
 package nz.ac.eitamazingtranslator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<String> germanTranslation = new ArrayList<>();
+        germanTranslation.add("Einz");
+        germanTranslation.add("Zwei");
+        germanTranslation.add("Einz");
+
+        List<String> frenchTranslation = new ArrayList<>();
+        frenchTranslation.add("Onze");
+        frenchTranslation.add("Einz");
+        frenchTranslation.add("Einz");
+
+        System.out.println("What is the number to translate?");
+        Scanner scanner = new Scanner(System.in);
+        String numberisstring = scanner.nextLine();
+        Integer number = null;
+        try {
+            number = Integer.parseInt(numberisstring);
+            System.out.println(number);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("The number has to be numeric");
+            System.exit(0);
+        }
+        System.out.println("What is the language: 1 is French, 2 is German?");
+
+        Integer option = null;
+        try {
+            option = Integer.parseInt(optionisstring);
+            System.out.println(option);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("The option has to be numeric");
+            System.exit(0);
+        }
+
+        if (option == 1) {
+            string translation = frenchTranslation.get(number - 1);
+        }
+
     }
 }
